8846b90e609287365fab0648dd7a8f8fd51d4e5e
Banu Sapakova <banuaksom@gmail.com>
2020-02-06 22:48:09 +0000

    first commit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,7 +7,7 @@ def top_ten(subreddit):
     req = requests.get("https://reddit.com/r/{}.json?sort=hot&limit=10".
                        format(subreddit), headers={"User-Agent": "custom"})
 
-    if(req.status_code == 200):
+    if (req.status_code == 200):
         for i in req.json().get("data").get("children"):
             print(i.get("data").get("title"))
     else:
